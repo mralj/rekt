28d5a1b649f6c28aeafb2a46cd03355a97e877e1
mralj <nikola.mratinic@gmail.com>
2023-02-25 00:15:04 +0100

    try to connect to nodes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use rekt::{constants::BOOTSTRAP_NODES, types::node_record::NodeRecord};
+use color_eyre::eyre::Result;
+use rekt::{constants::*, types::node_record::NodeRecord};
 use tokio::io::AsyncReadExt;
 use tokio::net::TcpStream;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let nodes = BOOTSTRAP_NODES
+async fn main() -> Result<()> {
+    color_eyre::install()?; // better errors
+
+    let nodes = RND_NODES
         .iter()
         .map(|n| n.parse().unwrap())
         .collect::<Vec<NodeRecord>>();
 
+    let mut tasks = Vec::new();
     for node in nodes {
-        tokio::spawn(async move {
+        let t = tokio::spawn(async move {
             let mut stream = match TcpStream::connect(node.get_socket_addr()).await {
                 Ok(stream) => stream,
                 Err(e) => {
@@ -19,10 +23,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             };
             // 5kb is random
-            let mut buf = [0; 5 * 1024 * 1024];
-            stream.read(&mut buf).await.unwrap();
-            println!("Received: {}", String::from_utf8_lossy(&buf));
+            let mut buf = [0; 100 * 1024];
+            loop {
+                match stream.read(&mut buf).await {
+                    Ok(0) => {
+                        println!("Connection closed");
+                        return;
+                    }
+                    Ok(_) => {
+                        println!("The server says {}", String::from_utf8_lossy(&buf));
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to read from socket: {}", e);
+                    }
+                }
+            }
         });
+        tasks.push(t);
+    }
+
+    for task in tasks {
+        task.await?;
     }
 
     Ok(())
