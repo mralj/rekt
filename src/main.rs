9a939b077d6629b830b810b47aedbe29edd046bd
mralj <nikola.mratinic@gmail.com>
2023-02-28 19:46:04 +0100

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,21 @@
 use color_eyre::eyre::Result;
-use rekt::{constants::*, types::node_record::NodeRecord};
-use tokio::io::AsyncReadExt;
-use tokio::net::TcpStream;
+use rekt::connection::connect_to_node;
+use rekt::constants::*;
+use tokio::task::JoinHandle;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?; // better errors
 
-    let nodes = RND_NODES
+    let connect_to_nodes_tasks = BOOTSTRAP_NODES
         .iter()
         .map(|n| n.parse().unwrap())
-        .collect::<Vec<NodeRecord>>();
+        .into_iter()
+        .map(move |n| connect_to_node(n))
+        .collect::<Vec<JoinHandle<()>>>();
 
-    let mut tasks = Vec::new();
-    for node in nodes {
-        let t = tokio::spawn(async move {
-            let mut stream = match TcpStream::connect(node.get_socket_addr()).await {
-                Ok(stream) => stream,
-                Err(e) => {
-                    eprintln!("Failed to connect: {}", e);
-                    return;
-                }
-            };
-            // 5kb is random
-            let mut buf = [0; 100 * 1024];
-            loop {
-                match stream.read(&mut buf).await {
-                    Ok(0) => {
-                        println!("Connection closed");
-                        return;
-                    }
-                    Ok(_) => {
-                        println!("The server says {}", String::from_utf8_lossy(&buf));
-                    }
-                    Err(e) => {
-                        eprintln!("Failed to read from socket: {}", e);
-                    }
-                }
-            }
-        });
-        tasks.push(t);
-    }
-
-    for task in tasks {
-        task.await?;
+    for t in connect_to_nodes_tasks {
+        t.await?;
     }
 
     Ok(())
