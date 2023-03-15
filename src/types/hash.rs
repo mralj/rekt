bfdc4ac8c449934e92564c6a63dd4149ca4d1ab9
mralj <nikola.mratinic@gmail.com>
2023-03-15 11:39:41 +0100

    added hello msg

--- src/types/hash.rs
+++ src/types/hash.rs
@@ -1,11 +1,13 @@
 use derive_more::{AsRef, Deref};
 use fixed_hash::construct_fixed_hash;
+use impl_serde::impl_fixed_hash_serde;
 use open_fastrlp::{RlpDecodableWrapper, RlpEncodableWrapper, RlpMaxEncodedLen};
 
 construct_fixed_hash! {
     #[derive(AsRef, Deref, RlpEncodableWrapper, RlpDecodableWrapper, RlpMaxEncodedLen)]
     pub struct H512(64);
 }
+impl_fixed_hash_serde!(H512, 64);
 
 construct_fixed_hash! {
     #[derive(AsRef, Deref, RlpEncodableWrapper, RlpDecodableWrapper, RlpMaxEncodedLen)]
