cb28fac8c3e5461ed70722bb35c47d88cc06200f
mralj <nikola.mratinic@gmail.com>
2023-06-03 12:34:06 +0200

    formatting fix

--- src/types/node_record.rs
+++ src/types/node_record.rs
@@ -123,7 +123,6 @@ mod test {
             id: "6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0".parse().unwrap(),
             pub_key: pk,
             str: url.to_string(),
-        
         })
     }
     #[test]
@@ -139,7 +138,6 @@ mod test {
             id: "6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0".parse().unwrap(),
             pub_key: pk,
             str: url.to_string(),
-        
         })
     }
 }
