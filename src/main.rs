f3d22e15576aa158105c40b80d558fb406565a62
mralj <nikola.mratinic@gmail.com>
2023-06-09 13:24:02 +0200

    run "in parallel"

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use futures::future::join_all;
+use futures::stream::FuturesUnordered;
+use futures::StreamExt;
 use secp256k1::{Secp256k1, SecretKey};
-use tokio::task::JoinHandle;
 
-use rekt::{constants::*, rlpx::connect_to_node, rlpx::RLPXSessionError};
+use rekt::{constants::*, rlpx::connect_to_node};
 use tracing::{error, Level};
 
 #[tokio::main]
@@ -15,20 +15,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let secret_key = SecretKey::new(&mut secp256k1::rand::thread_rng());
     let public_key = secp256k1::PublicKey::from_secret_key(&Secp256k1::new(), &secret_key);
-    let connect_to_nodes_tasks = OUR_NODE
+
+    let mut connect_to_nodes_tasks: FuturesUnordered<_> = RND_NODES
         .iter()
         .map(|n| n.parse().unwrap())
         .map(|n| connect_to_node(n, secret_key, public_key))
-        .collect::<Vec<JoinHandle<Result<(), RLPXSessionError>>>>();
+        .collect();
 
-    join_all(connect_to_nodes_tasks)
-        .await
-        .iter()
-        .for_each(|task_result| {
-            if let Ok(Err(e)) = task_result {
-                error!("{}", e)
-            }
-        });
+    while let Some(task_result) = connect_to_nodes_tasks.next().await {
+        if let Ok(Err(e)) = task_result {
+            error!("{}", e)
+        }
+    }
 
     Ok(())
 }
