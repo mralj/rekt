78fe29b769c30f0309a857598b0f9cef637fdc43
mralj <nikola.mratinic@gmail.com>
2023-07-07 18:38:23 +0200

    test 2

--- src/server/outbound_connections.rs
+++ src/server/outbound_connections.rs
@@ -146,7 +146,7 @@ impl OutboundConnections {
         loop {
             select! {
                 _ = count_interval.tick() => {
-                    println!("{}", PEERS.len());
+                    println!("{}", PEERS_BY_IP.len());
                 },
                 _ = info_interval.tick() => {
                     tracing::info!("==================== ==========================  ==========");
