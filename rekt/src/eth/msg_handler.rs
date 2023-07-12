2ae95a98474d70e83241714dfb7f1c068e95640d
mralj <nikola.mratinic@gmail.com>
2023-07-12 23:47:53 +0200

    test

--- rekt/src/eth/msg_handler.rs
+++ rekt/src/eth/msg_handler.rs
@@ -42,10 +42,7 @@ fn handle_tx_hashes(msg: Message) -> Result<Option<Message>, ETHError> {
 
     let anno_hashes: Vec<H256> = Vec::decode(&mut &msg.data[..])?;
 
-    let hashes = anno_hashes
-        .iter()
-        .filter(|h| !TX_HASHES.contains(h))
-        .collect();
+    let hashes = anno_hashes.iter().filter(|_| true).collect();
 
     Ok(Some(Message {
         id: Some(25),
