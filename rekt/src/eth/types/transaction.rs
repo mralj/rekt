2b7e00790c94cd9ae3231fe460501b161b06213c
mralj <nikola.mratinic@gmail.com>
2023-07-21 12:47:54 +0200

    test rlp decoding

--- rekt/src/eth/types/transaction.rs
+++ rekt/src/eth/types/transaction.rs
@@ -154,13 +154,7 @@ impl Transaction {
 
         let recipient = H160::decode(payload_view)?;
 
-        unsafe {
-            let d = (Instant::now() - msg_received_at).as_micros();
-            SUM += d;
-            CNT += 1;
-            MIN = if d < MIN { d } else { MIN };
-            MAX = if d > MAX { d } else { MAX };
-        }
+        
 
         // if recipient == H160::from_str("0x13f4EA83D0bd40E75C8222255bc855a974568Dd4").unwrap() {
         //     let timestamp = SystemTime::now()
