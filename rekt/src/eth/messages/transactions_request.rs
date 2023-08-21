f4c31cf3a914b665d28db92d6240b776eb575090
mralj <nikola.mratinic@gmail.com>
2023-08-21 13:13:36 +0200

    added transactions request

---
+++ rekt/src/eth/messages/transactions_request.rs
@@ -0,0 +1,22 @@
+use crate::types::hash::H256;
+
+use bytes::BytesMut;
+use open_fastrlp::{Encodable, RlpEncodable};
+
+#[derive(Debug, Clone, PartialEq, Eq, RlpEncodable)]
+pub struct TransactionsRequest {
+    id: u64,
+    hashes: Vec<H256>,
+}
+
+impl TransactionsRequest {
+    pub fn new(hashes: Vec<H256>) -> Self {
+        Self { id: 0, hashes }
+    }
+
+    pub fn rlp_encode(&self) -> BytesMut {
+        let mut rlp = BytesMut::new();
+        self.encode(&mut rlp);
+        rlp
+    }
+}
