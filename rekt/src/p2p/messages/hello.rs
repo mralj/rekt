99c2b28a34d0dae1080a8c25b9443f524fc617df
mralj <nikola.mratinic@gmail.com>
2023-08-25 17:56:32 +0200

    use default port when establishing connection

--- rekt/src/p2p/messages/hello.rs
+++ rekt/src/p2p/messages/hello.rs
@@ -5,11 +5,11 @@ use once_cell::sync::OnceCell;
 use open_fastrlp::{Encodable, RlpDecodable, RlpEncodable};
 use serde::{Deserialize, Serialize};
 
+use crate::constants::DEFAULT_PORT;
 use crate::p2p::protocol::Protocol;
 use crate::types::hash::H512;
 
 const DEFAULT_P2P_PROTOCOL_VERSION: usize = 5;
-const DEFAULT_PORT: usize = 30311;
 static OUR_HELLO_MESSAGE_RLP_ENCODED: OnceCell<BytesMut> = OnceCell::new();
 
 /// Message used in the `p2p` handshake, containing information about the supported RLPx protocol
@@ -29,7 +29,7 @@ pub struct HelloMessage {
     // But! GETH node also prints for the majority of nodes this field as 0
     // What's more, in the GETH code I don't see that when we are sending hello message this field
     // is even set
-    pub port: usize,
+    pub port: u16,
     /// The secp256k1 public key corresponding to the node's private key.
     pub id: H512,
 }
