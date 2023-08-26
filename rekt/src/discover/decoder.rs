854f8b50c2c7b695a76e95641b0ccb388fde3f01
mralj <nikola.mratinic@gmail.com>
2023-08-26 11:17:44 +0200

    cleanup 1

--- rekt/src/discover/decoder.rs
+++ rekt/src/discover/decoder.rs
@@ -1,14 +1,16 @@
-const MAC_SIZE: usize = 32;
-const SIG_SIZE: usize = 65;
-const HEAD_SIZE: usize = MAC_SIZE + SIG_SIZE;
-
+// The following constants are defined in the "docs"
+// https://github.com/ethereum/devp2p/blob/master/discv4.md
+const HASH_SIZE: usize = 32;
+const SIGNATURE_SIZE: usize = 65;
+const HEADER_SIZE: usize = HASH_SIZE + SIGNATURE_SIZE;
 // Discovery packets are defined to be no larger than 1280 bytes.
 // Packets larger than this size will be cut at the end and treated
 // as invalid because their hash won't match.
 const MAX_PACKET_SIZE: usize = 1280;
+const MESSAGE_TYPE_POSITION: usize = HEADER_SIZE + 1;
 
 pub fn packet_size_is_valid(size: usize) -> bool {
-    if size <= HEAD_SIZE {
+    if size <= HEADER_SIZE {
         return false;
     }
 
@@ -19,8 +21,8 @@ pub fn packet_size_is_valid(size: usize) -> bool {
     true
 }
 
-pub fn decode_msg(buf: &[u8]) {
-    let msg_type = buf[HEAD_SIZE..][0];
+pub fn decode_msg_type(buf: &[u8]) {
+    let msg_type = buf[MESSAGE_TYPE_POSITION];
 
     match msg_type {
         1 => println!("Ping"),
