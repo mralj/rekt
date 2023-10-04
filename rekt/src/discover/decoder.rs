a733d644a5a896c82af52a0cefed2640d0fb7de8
mralj <nikola.mratinic@gmail.com>
2023-10-04 12:32:14 +0200

    decode node_id from discover message

--- rekt/src/discover/decoder.rs
+++ rekt/src/discover/decoder.rs
@@ -2,12 +2,14 @@ use std::net::SocketAddr;
 
 use chrono::Local;
 use enr::Enr;
+use ethers::utils::keccak256;
 use open_fastrlp::Decodable;
-use secp256k1::SecretKey;
+use secp256k1::ecdsa::{RecoverableSignature, RecoveryId};
+use secp256k1::{SecretKey, SECP256K1};
 
 use crate::blockchain::bsc_chain_spec::BSC_MAINNET_FORK_FILTER;
 use crate::discover::messages::find_node::Neighbours;
-use crate::types::hash::H256;
+use crate::types::hash::{H256, H512};
 
 use super::messages::discover_message::{DiscoverMessage, DiscoverMessageType};
 use super::messages::enr::EnrResponse;
@@ -30,9 +32,43 @@ pub fn decode_msg_and_create_response(
     enr: &Enr<SecretKey>,
 ) -> Option<DiscoverMessage> {
     let hash = &buf[..HASH_SIZE];
-    let _signature = &buf[HASH_SIZE..HEADER_SIZE];
-    let msg_type = &buf[HEADER_SIZE..][0];
+    let signature = &buf[HASH_SIZE..HEADER_SIZE - 1];
+    let msg_type = &buf[HEADER_SIZE];
     let msg_data = &mut &buf[HEADER_SIZE + TYPE_SIZE..];
+
+    let recovery_id = match RecoveryId::from_i32(buf[HEADER_SIZE - 1] as i32) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Could not parse recovery id: {:?}", e);
+            return None;
+        }
+    };
+
+    let recoverable_sig = match RecoverableSignature::from_compact(signature, recovery_id) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Could not parse recoverable signature: {:?}", e);
+            return None;
+        }
+    };
+
+    let msg = match secp256k1::Message::from_slice(&keccak256(&buf[97..])) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Could not parse message: {:?}", e);
+            return None;
+        }
+    };
+
+    let pk = match SECP256K1.recover_ecdsa(&msg, &recoverable_sig) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Could not recover public key: {:?}", e);
+            return None;
+        }
+    };
+
+    let _node_id = H512::from_slice(&pk.serialize_uncompressed()[1..]);
 
     let msg_type = DiscoverMessageType::try_from(*msg_type).ok()?;
     if !msg_type.discover_msg_should_be_handled() {
