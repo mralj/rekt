78cb234af577c06af54a96e5beac5ae62b56e47c
mralj <nikola.mratinic@gmail.com>
2023-10-07 18:04:14 +0200

    proper json names

--- rekt/src/token/mod.rs
+++ rekt/src/token/mod.rs
@@ -7,18 +7,25 @@ type TxSignatureHash = ethers::types::H32;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Token {
+    #[serde(rename = "buyToken")]
     pub buy_token_address: Address,
+    #[serde(rename = "liqToken")]
     pub liquidity_token_address: Address,
 
+    #[serde(rename = "buyBNB")]
     pub buy_amount: f64,
+    #[serde(rename = "testPercent")]
     pub protection_percent: u16,
 
+    #[serde(rename = "enableBuyConfig")]
     pub enable_buy_config: EnableBuyConfig,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct EnableBuyConfig {
+    #[serde(rename = "to")]
     pub tx_to: Address,
+    #[serde(rename = "txHash")]
     pub enable_buy_tx_hash: TxSignatureHash,
 }
 
@@ -37,13 +44,13 @@ mod test {
     #[test]
     fn parse_token() {
         let json = r#"{
-        "buy_token_address": "0xaE01f96CB9ce103A6A1297CC19EC0d0814Cf4c7F",
-        "liquidity_token_address": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-        "buy_amount": 100.5,
-        "protection_percent": 10,
-        "enable_buy_config": {
-            "tx_to": "0xCF4217DB0Ea759118d5218eFdCE88B5822859D62",
-            "enable_buy_tx_hash": "0x7d315a2e"
+        "buyToken": "0xaE01f96CB9ce103A6A1297CC19EC0d0814Cf4c7F",
+        "liqToken": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+        "buyBNB": 100.5,
+        "testPercent": 10,
+        "enableBuyConfig": {
+            "to": "0xCF4217DB0Ea759118d5218eFdCE88B5822859D62",
+            "txHash": "0x7d315a2e"
         }
       }"#;
 
