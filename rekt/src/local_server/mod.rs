23a27dbab6eca46eeb22e295f43b8863d38648df
mralj <nikola.mratinic@gmail.com>
2023-10-18 14:20:02 +0200

    bugfix

--- rekt/src/local_server/mod.rs
+++ rekt/src/local_server/mod.rs
@@ -1,6 +1,6 @@
 use warp::Filter;
 
-pub async fn run_local_server() {
+pub fn run_local_server() {
     tokio::task::spawn(async move {
         let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
         warp::serve(hello).run(([0, 0, 0, 0], 6060)).await;
