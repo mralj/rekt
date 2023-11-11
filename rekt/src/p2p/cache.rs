40680f664803918b07c6c80b526af022c5c19264
mralj <nikola.mratinic@gmail.com>
2023-11-11 18:01:08 +0100

    updated xxhash cahce init

--- rekt/src/p2p/cache.rs
+++ rekt/src/p2p/cache.rs
@@ -3,9 +3,8 @@ use std::hash::BuildHasherDefault;
 use bytes::Bytes;
 use dashmap::DashMap;
 use static_init::dynamic;
-use xxhash_rust::xxh3;
+use xxhash_rust::xxh3::{self, Xxh3Builder};
 
-type DashMapWithXxHash<K, V> = DashMap<K, V, BuildHasherDefault<xxh3::Xxh3>>;
 #[dynamic]
-pub(super) static CACHE: DashMap<Bytes, ()> =
-    DashMap::with_capacity_and_shard_amount(1_000_000, 256);
+pub(super) static CACHE: DashMap<Bytes, (), Xxh3Builder> =
+    DashMap::with_capacity_and_hasher_and_shard_amount(1_000_000, Xxh3Builder::new(), 256);
