2080b1e2032177f1e5b6fa5aa326441088ea3b7c
mralj <nikola.mratinic@gmail.com>
2023-11-11 18:48:13 +0100

    default hasher

--- rekt/src/p2p/cache.rs
+++ rekt/src/p2p/cache.rs
@@ -6,5 +6,5 @@ use static_init::dynamic;
 use xxhash_rust::xxh3::{self, Xxh3Builder};
 
 #[dynamic]
-pub(super) static CACHE: DashMap<Bytes, (), Xxh3Builder> =
-    DashMap::with_capacity_and_hasher_and_shard_amount(1_000_000, Xxh3Builder::new(), 256);
+pub(super) static CACHE: DashMap<Bytes, ()> =
+    DashMap::with_capacity_and_shard_amount(1_000_000, 256);
