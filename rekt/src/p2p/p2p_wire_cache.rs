dcbaaed90b05c81c17eb36dd2bc4010ebc047e9d
mralj <nikola.mratinic@gmail.com>
2023-11-30 17:30:24 +0100

    use same cache for both full txs and annos

--- rekt/src/p2p/p2p_wire_cache.rs
+++ rekt/src/p2p/p2p_wire_cache.rs
@@ -15,32 +15,28 @@ pub fn init_cache() {
             CACHE_TXS.push(false);
         }
 
-        CACHE_HASHES.reserve_exact(u32::MAX as usize);
-        for _ in 0..u32::MAX {
-            CACHE_HASHES.push(false);
-        }
+        // CACHE_HASHES.reserve_exact(u32::MAX as usize);
+        // for _ in 0..u32::MAX {
+        //     CACHE_HASHES.push(false);
+        // }
     }
     println!("P2P wire cache initialized");
 }
 
-pub(super) fn insert_hash(data: &Bytes) -> bool {
-    let index = hash(data);
-    unsafe {
-        if CACHE_HASHES[index] == ALREADY_CACHED {
-            ALREADY_CACHED
-        } else {
-            CACHE_HASHES[index] = ALREADY_CACHED;
-            NOT_CACHED
-        }
-    }
-}
+// pub(super) fn insert_hash(data: &Bytes) -> bool {
+//     let index = hash(data);
+//     unsafe {
+//         if CACHE_HASHES[index] == ALREADY_CACHED {
+//             ALREADY_CACHED
+//         } else {
+//             CACHE_HASHES[index] = ALREADY_CACHED;
+//             NOT_CACHED
+//         }
+//     }
+// }
 
 pub(super) fn insert_tx(data: &Bytes) -> bool {
     let index = hash(data);
-    if index >= u32::MAX as usize {
-        println!("index out of range");
-        return false;
-    }
     unsafe {
         if CACHE_TXS[index] == ALREADY_CACHED {
             return ALREADY_CACHED;
